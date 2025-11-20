1da0fe6681215de2b6baed9330135930487c00cc
EvOps007 <even.kr@hotmail.com>
2025-11-20 13:19:26 +0100

    netlify spesifikasjoner

--- app/component/SpillMesterSide.js
+++ app/component/SpillMesterSide.js
@@ -8,10 +8,14 @@ import { useRouter } from "next/navigation";
 export default function SpillMesterSide({ gameCode }) {
     const [hostName, setHostName] = useState("")
     const [players, setPlayers] = useState([])
-    const searchParams = useSearchParams();
-    const gameCodeFromUrl = searchParams.get("code");
+    const [gameCode, setGameCode] = useState("");
     const name = hostName
     const router = useRouter();
+
+    useEffect(() => {
+        const params = new URLSearchParams(window.location.search);
+        setGameCode(params.get("code") || "");
+    }, []);
 
     const fetchPlayers = async () => {
         try {
